cbd3390b1958ab311b638f206ef640e3db6ce5ba
alexander-p30 <alexanderp30e@gmail.com>
2020-08-24 11:17:35 -0300

    :art: Transforms repeated code into lib functions
    Several improvements to unnecessary code repetition and improved modularization.

--- libs/functions_lib.rb
+++ libs/functions_lib.rb
@@ -1,16 +1,26 @@
+# frozen_string_literal: true
+
+MAXIMUM_DIGITS = 10
+
+# Adds method for proper rounding
+class Float
+  def round_max_digits
+    round(MAXIMUM_DIGITS - to_i.to_s.length)
+  end
+end
+
 def fetch_function_data
-  func_degree = 0
-  coefficients = []
+  function = {}
   loop do
-    func_degree, coefficients = mount_function
+    function = mount_function
     print 'Does your equation look like this? '
-    display_function(func_degree, coefficients)
+    display_function(function)
     print " = 0\ny/n: "
     break if gets.chomp.strip == 'y'
 
     system('clear')
   end
-  [func_degree, coefficients]
+  function
 end
 
 def mount_function
@@ -23,12 +33,12 @@ def mount_function
     coefficients.unshift(gets.to_i)
   end
 
-  [func_degree, coefficients]
+  { degree: func_degree, coefficients: coefficients }
 end
 
-def display_function(func_degree, coefficients)
-  func_degree.downto(0) do |i|
-    coefficient = coefficients[i]
+def display_function(function)
+  function[:degree].downto(0) do |i|
+    coefficient = function[:coefficients][i]
     func_variable = i.positive? && coefficient != 0 ? "x^#{i}" : ''
     signal = if coefficient.zero? || coefficient == 1
                coefficient = ''
@@ -38,5 +48,65 @@ def display_function(func_degree, coefficients)
              end
     print "#{signal}#{coefficient.to_i.negative? ? coefficient * -1 : coefficient}#{func_variable}"
   end
-  nil
+end
+
+def calculate_function(function, variable_value)
+  result = 0
+  function[:degree].downto(0).each do |i|
+    result += (variable_value**i) * function[:coefficients][i]
+  end
+  result
+end
+
+def calculate_new_limit(data, method)
+  methods = { bissection: :new_limit_bissection, false_position: :new_limit_false_position }
+  new_limit = send(methods[method], { function: data[:function], limits: data[:limits] })
+  new_limit.round_max_digits
+end
+
+def new_limit_bissection(function_and_limits)
+  (function_and_limits[:limits][1] + function_and_limits[:limits][0]) / 2
+end
+
+def new_limit_false_position(function_and_limits)
+  function = function_and_limits[:function]
+  limits = function_and_limits[:limits]
+
+  higher_result = calculate_function(function, limits[1])
+  a = limits[0] * higher_result
+  lower_result = calculate_function(function, limits[0])
+  b = limits[1] * lower_result
+
+  (a - b) / (higher_result - lower_result)
+end
+
+def rearrange_limits(limits, function, func_root)
+  approx_root_result = calculate_function(function, func_root)
+  lower_limit_result = calculate_function(function, limits[0])
+
+  if (lower_limit_result * approx_root_result).negative?
+    limits[1] = func_root
+  else
+    limits[0] = func_root
+  end
+
+  limits
+end
+
+def find_approximate_zero(data, method)
+  func_root = 0
+  k = 0
+  result = ''
+  current_iteration_data = {}
+  while !send(data[:stopping_criterion], current_iteration_data) && k != data[:maximum_iterations]
+    k += 1
+    func_root = calculate_new_limit(data, method)
+    data[:limits] = rearrange_limits(data[:limits], data[:function], func_root)
+    current_iteration_data = { limits: data[:limits],
+                               error: data[:error_margin],
+                               result: result,
+                               iteration: k,
+                               max_iterations: data[:maximum_iterations] }
+  end
+  [func_root, k, data[:limits]]
 end
--- libs/problems_lib.rb
+++ libs/problems_lib.rb
@@ -1,20 +1,31 @@
 # frozen_string_literal: true
 
-MAXIMUM_DIGITS = 10
-STOPPING_CRITERIA = %i[stop_by_result stop_by_difference stop_by_any].freeze
+STOPPING_CRITERIA = { result: :stop_by_result,
+                      difference: :stop_by_difference,
+                      iterations: :stop_by_iteration_number,
+                      any: :stop_by_any }.freeze
+
+CRITERIA_DESCRIPTION = { result: '|f(x)| < E',
+                         difference: '|X[k] - X[k-1] < E',
+                         iterations: 'By reaching maximum number of iterations',
+                         any: 'Any of the above' }.freeze
 
 def stop_by_result(calc_data)
   result = calc_data[:result]
   error = calc_data[:error]
   return false if result.is_a? String
 
-  result.negative? ? (result * -1) < error : result < error
+  result.negative? ? (result * -1) <= error : result <= error
 end
 
 def stop_by_difference(calc_data)
   diff = calc_data[:limits][1] - calc_data[:limits][0]
   error = calc_data[:error]
-  diff.negative? ? (diff * -1) < error : diff < error
+  diff.negative? ? (diff * -1) <= error : diff <= error
+end
+
+def stop_by_iteration_number(calc_data)
+  calc_data[:iteration] >= calc_data[:max_iterations]
 end
 
 def stop_by_any(calc_data)
@@ -22,27 +33,30 @@ def stop_by_any(calc_data)
 end
 
 def fetch_problem_data
-  print('Please state the lower limit: ')
-  lower_limit = gets.to_f
+  limits = []
+  print 'Please state the lower limit: '
+  limits << gets.to_f
+  print 'Please state the higher limit: '
+  limits << gets.to_f
 
-  print('Please state the higher limit: ')
-  higher_limit = gets.to_f
-
-  print('Please state the error margin`s exponent (10^?): ')
+  print 'Please state the error margin`s exponent (10^?): '
   error_margin = 10**gets.to_f
 
-  print('Please state the maximum number of iterations (-1 for unlimited): ')
+  print 'Please state the maximum number of iterations (-1 for unlimited): '
   maximum_iterations = gets.to_i
 
-  print("What are the stopping criteria?\n1. |f(x)| < E\n2. |X[k] - X[k-1] < E\n3. both\nChoose your answer: ")
-  stopping_criteria = gets.to_i - 1
+  print 'What are the stopping criteria?'
+  CRITERIA_DESCRIPTION.values.each_with_index do |criterion_description, index|
+    puts "#{index}. #{criterion_description}"
+  end
+  print('Choose your answer: ')
+  criterion = STOPPING_CRITERIA.keys[gets.to_i - 1]
 
-
-  [[lower_limit, higher_limit], error_margin, maximum_iterations, stopping_criteria]
+  [limits.sort, error_margin, maximum_iterations, criterion]
 end
 
-def display_results(func_root, lower_limit, higher_limit, iterations)
+def display_results(func_root, iterations, limits)
   puts '----------------------------RESULTS----------------------------'
-  puts "The new interval is [#{lower_limit}, #{higher_limit}], after #{iterations} iterations."
+  puts "The new interval is #{limits}, after #{iterations} iterations."
   puts "The approximate root is #{func_root}."
 end
